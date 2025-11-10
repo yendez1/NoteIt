f57894a55f1aa43d0d60cafe857dddf95c96e794
yendez1 <gringinoir@gmail.com>
2025-11-09 19:22:33 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,3 @@
-const CACHE='noteit-cache-u4';
+const CACHE='noteit-cache-u7';
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./style.css','./app.js','./manifest.webmanifest','./assets/appicon.png','./assets/appicon-192.png','./assets/appicon-512.png','./assets/apple-touch-icon-180.png','./assets/favicon-32.png'])))});
 self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))});
