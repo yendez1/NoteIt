c9afa38d95173f9fe43801b87f473233481a842f
yendez1 <gringinoir@gmail.com>
2025-12-02 19:09:02 -0600

    Create PlaylistInfo.cs

---
+++ PlaylistInfo.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NoteIt
+{
+    public class PlaylistInfo
+    {
+        public string Name { get; set; } = "";
+        public List<string> Tracks { get; set; } = new();
+    }
+
+
+}
+
